4085ed01d87f5f85754eb261e6fd315706867c31
yujinkim0819 <a76363282@gmail.com>
2022-07-27 13:33:34 +0900

    ejs를 통한 데이터 전달

---
+++ nodejs03/ejs를통한데이터전달.js
@@ -0,0 +1,17 @@
+// 모듈을 추출합니다
+var http = require('http');
+var fs = require('fs');
+var ejs = require('ejs');
+
+// 서버를 생성하고 실행합니다
+http.createServer(function (request, response) {
+    fs.readFile('ejsPage_ejs를통한데이터전달.ejs', 'utf-8', function (error, data) {
+        response.writeHead(200, {'Content-Type': 'text/html;charset-utf-8'});
+        response.end(ejs.render(data, {
+            name: 'funny_node.js',
+            description: 'Hello ejs With Node.js .. !'
+        }));
+    });        
+}).listen(4444, function() {
+    console.log('Server Running at http://127.0.0.1:4444');
+});

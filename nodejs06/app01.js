cd2d021c41ca32a8880559bf3ca632b429f97b33
yujinkim0819 <a76363282@gmail.com>
2022-08-02 09:38:09 +0900

    mysql 사용하기

--- nodejs06/app01.js
+++ nodejs06/app01.js
@@ -19,8 +19,21 @@ var client = mysql.createConnection({
     password: '1111',
     database: 'company'
 });
-//데이터 표시
+
+//서버를 실행합니다
+app.listen(4444, function () {
+    console.log('server running at http://127.0.0.1:4444');
+})
+//데이터 삭제
+app.get('/delete/:id', function (req, res) {
+    client.query('delete from produts where id=?', [req.params.id],
+    function () {
+        res.redirect('/')
+    })
+})
+// 라우트를 수행합니다
 app.get('/',function (req, res) {
+    // 파일을 읽습니다
     fs.readFile('list.html','uft8',function (err,data) {
         client.query('select * from product', function (error, results) {
             res.send(ejs.render(data,{
